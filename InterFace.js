01fd0088c6d361064cd8fdd386db59668c4852f7
Kavita <kkavita92@gmail.com>
2017-06-07 16:38:35 +0100

    - Link buttons to thermostat model using JQuery

--- InterFace.js
+++ InterFace.js
@@ -1,6 +1,30 @@
-$('body').click(function() {
-	$(this).css('background-color','#01FF70')
+$(function() {
+var thermostat = new Thermostat();
+
+$('#temperature').text(thermostat.temperature);
+
+$('#regulate_temp_up').click(function() {
+	thermostat.up(1);
+	$('#temperature').text(thermostat.temperature);
 });
-$('#regulate_temp').click(function() {
-	alert('You\'re Awesome!')
+
+$('#regulate_temp_down').click(function() {
+	thermostat.down(1);
+	$('#temperature').text(thermostat.temperature);
+});
+
+$('#power_save_status').text(thermostat.isPowerSaving);
+
+$('#power_save').click(function() {
+	thermostat.noPowerSaving();
+	$('#power_save_status').text(thermostat.isPowerSaving);
+});
+
+$('#reset').click(function() {
+	thermostat.reset();
+	$('#temperature').text(thermostat.temperature);
+});
+
+
+
 });
