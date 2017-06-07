3ece7efec4a4f60d25117dd7b77d7d0df65f9c88
RSijelmass <R.Sijelmass@gmail.com>
2017-06-07 18:11:20 +0100

    Start including AJAX in jquery - get API from London weather - is printing something in console, not what we want - isn't printing anything on app

--- InterFace.js
+++ InterFace.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 var thermostat = new Thermostat();
 updateTemperature();
 
+$.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a012ce56b3fea36ffd408256d4eeb21a', function(data) {
+		console.log(data.weather.main);
+		$('#weather').text(data);
+});
+
 $('#regulate_temp_up').click(function() {
 	thermostat.up(1);
 	updateTemperature();
@@ -15,7 +20,7 @@ $('#regulate_temp_down').click(function() {
 $('#power_save_status').text(thermostat.isPowerSaving);
 
 $('#power_save').click(function() {
-	thermostat.noPowerSaving();
+	thermostat.switchPowerSaving();
 	$('#power_save_status').text(thermostat.isPowerSaving);
 });
 
